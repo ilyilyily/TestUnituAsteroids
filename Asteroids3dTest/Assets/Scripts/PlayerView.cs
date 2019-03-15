7c122146c5ee1835a73a5e5374bd3cad8231fddc
ilyilyily@mail.ru <Leninidze10>
2019-03-15 19:38:34 +0300

    sdd script to control ship (without mvc now)

---
+++ Asteroids3dTest/Assets/Scripts/PlayerView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis { "Horizontal"};
+        float MoveVertical = Input.GetAxis { "Vertical"};
+
+        Vector3 movement new Vector3 { MoveHorizontal , 0.0f, MoveVertical }
+        rigidbody.velocity = movement;
+    }
+}
