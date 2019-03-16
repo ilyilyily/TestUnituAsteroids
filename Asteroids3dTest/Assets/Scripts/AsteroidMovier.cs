e8c90d5301fadd6bb241524f1393f380bab2fd89
ilyilyily@mail.ru <Leninidze10>
2019-03-16 21:12:38 +0300

    trying to move asteroid

---
+++ Asteroids3dTest/Assets/Scripts/AsteroidMovier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovier : MonoBehaviour
+{
+    private float asteroid_speed = 10;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * asteroid_speed * -1;
+    }
+}
--- Asteroids3dTest/Assets/Scripts/BoltModel.cs
+++ Asteroids3dTest/Assets/Scripts/BoltModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoltModel : MonoBehaviour
+public class BoltModel
 {
     public static float bolt_speed = 10;
     
